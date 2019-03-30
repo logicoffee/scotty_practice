e3508e34f59976c8a48db77d8af9923958030694
logicoffee <riemann1618@gmail.com>
2019-03-30 15:16:59 +0900

    Add types for insertion

--- src/Model/AppUser/Entity.hs
+++ src/Model/AppUser/Entity.hs
@@ -2,18 +2,21 @@
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE TemplateHaskell       #-}
 
 module Model.AppUser.Entity where
 
-import           Data.Text                       (Text)
-import           Database.HDBC.Query.TH          (defineTableFromDB',
-                                                  makeRelationalRecord)
-import           Database.HDBC.Schema.Driver     (Driver (..))
-import           Database.HDBC.Schema.PostgreSQL (driverPostgreSQL)
-import           Database.Relational.Pi          (Pi)
-import           GHC.Generics                    (Generic)
-import           Model.DB                        (connectPG)
+import           Data.Functor.ProductIsomorphic.Class ((|$|), (|*|))
+import           Data.Text                            (Text)
+import           Database.HDBC.Query.TH               (defineTableFromDB',
+                                                       makeRelationalRecord)
+import           Database.HDBC.Schema.Driver          (Driver (..))
+import           Database.HDBC.Schema.PostgreSQL      (driverPostgreSQL)
+import           Database.Relational.Pi               (Pi)
+import           GHC.Generics                         (Generic)
+import           Model.DB                             (connectPG)
+import           Util
 
 defineTableFromDB'
     connectPG
@@ -24,13 +27,25 @@ defineTableFromDB'
     [''Show, ''Generic]
 
 data AppUser' = AppUser'
-    { name         :: !Text
-    , passwordHash :: !Text
+    { pName         :: !Text
+    , pPasswordHash :: !Text
     } deriving (Generic)
+
+data TmpAppUser = TmpAppUser
+    { tmpName              :: !Text
+    , password             :: !Text
+    , passwordConfirmation :: !Text
+    }
+
+makeRelationalRecord ''AppUser'
 
 piAppUser :: Pi AppUser AppUser'
 piAppUser = AppUser'
     |$| name'
     |*| passwordHash'
 
-makeRelationalRecord ''AppUser'
+-- TODO: バリデーションの実装
+makeAppUser' :: TmpAppUser -> Either [Text] AppUser'
+makeAppUser' (TmpAppUser name ps psConf)
+    | ps == psConf = Right $ AppUser' name $ hashPassword ps
+    | otherwise    = Left ["password confirmation doesn't match password"]
