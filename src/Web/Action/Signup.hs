fd70740fe380dcd8055a4109e571d70d480b7a7a
logicoffee <riemann1618@gmail.com>
2019-03-31 18:47:21 +0900

    Make api return json values

--- src/Web/Action/Signup.hs
+++ src/Web/Action/Signup.hs
@@ -2,6 +2,9 @@
 module Web.Action.Signup where
 
 import           Control.Monad.IO.Class (liftIO)
+import           Data.Aeson.Types
+import           Data.Scientific        (scientific)
+import qualified Data.Text.Lazy         as T
 import           Model.AppUser.Entity
 import           Model.AppUser.Insert
 import           Web.Scotty
@@ -14,5 +17,5 @@ signupAction = do
         <*> param "password_confirmation"
     foo <- liftIO $ trySignup tmpU
     case foo of
-        Left msgs -> text $ mconcat msgs
-        Right uid -> text "uidを含んだjson" -- クッキーをセット
+        Left msgs -> json $ object ["errors" .= map T.toStrict msgs]
+        Right uid -> json $ object ["user_id" .= Number (scientific (toInteger uid) 0)]
