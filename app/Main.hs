ebb178a501ae26a456a8ed692c83390bd86e8573
logicoffee <riemann1618@gmail.com>
2019-04-06 19:23:34 +0900

    Introduce session middleware

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,21 @@
 module Main where
 
+import           Data.Acid
+import           Data.ByteString                (ByteString)
+import           Data.Text                      (Text)
+import qualified Data.Vault.Lazy                as V
+import           Network.Wai.Session            (Session)
 import           Web.Route
 import           Web.Scotty
+import           Web.ServerSession.Backend.Acid
+import           Web.ServerSession.Frontend.Wai
 
 main :: IO ()
-main = scotty 3000 route
+main = do
+    key <- V.newKey :: IO (V.Key (Session IO Text ByteString))
+    sto <- AcidStorage <$> openLocalState emptyState
+    session <- withServerSession key id sto
+
+    scotty 3000 $ do
+        middleware session
+        route
